47b5903766c0a2d2af9f884365be880e9c10a5ad
rii <88538677+riivx@users.noreply.github.com>
2024-03-01 00:46:12 +0200

    Create make_deps.mjs

---
+++ scripts/make_deps.mjs
@@ -0,0 +1,31 @@
+import { readdir, readFile, writeFile } from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { write } from "./make_defs.mjs";
+
+const dir = dirname(fileURLToPath(import.meta.url));
+
+const data = {};
+
+for (const lang of await readdir(".")) {
+  if (lang === "base" || lang.endsWith(".json")) continue;
+
+  for (const file of await readdir(join(".", lang))) {
+    const key = file.split(".")[0];
+    data[key] ??= {};
+    data[key][lang] = JSON.parse(await readFile(join(".", lang, file), "utf8"));
+  }
+}
+
+for (const [plugin, dt] of Object.entries(data)) {
+  await writeFile(
+    join(dir, "../lang", "strings.json"),
+    JSON.stringify(dt),
+  );
+
+  console.log(`Wrote strings.json`);
+}
+
+write(join(".", "base"), join(dir, "..", "lang", "defs.d.ts"));
+
+console.log("Done");
