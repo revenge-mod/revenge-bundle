b3a318a8c7aaa80e40d7679570cb544c8ff213c0
rii <88538677+riivx@users.noreply.github.com>
2024-03-02 17:10:07 +0200

    fix: crowdin builds

--- src/ui/settings/index.ts
+++ src/ui/settings/index.ts
@@ -1,8 +1,8 @@
 import patchPanels from "@ui/settings/patches/panels";
 import patchYou from "@ui/settings/patches/you";
-import { Lang } from "scripts/lang";
+//import { Lang } from "scripts/lang";
 
-export const lang = new Lang();
+//export const lang = new Lang();
 
 export default function initSettings() {
     const patches = [
