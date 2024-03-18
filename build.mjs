00a188261be0039e8561669a6f5896159d341559
poob1 <ddnoidenis@gmail.com>
2024-03-18 14:45:35 +0200

    [Build] Rename to revengedev.js from revengetest.js

--- build.mjs
+++ build.mjs
@@ -20,7 +20,7 @@ const commit =
 try {
   await build({
     entryPoints: ["./src/entry.ts"],
-    outfile: "./dist/revengetest.js",
+    outfile: "./dist/revengedev.js",
     minify: true,
     bundle: true,
     format: "iife",
