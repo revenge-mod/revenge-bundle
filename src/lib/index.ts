b3457625c62888a0857000c8e95ab2892787e227
amsyarasyiq <82711525+amsyarasyiq@users.noreply.github.com>
2024-09-08 00:13:06 +0800

    chore!: competely remove bunny.managers

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,30 +1,13 @@
 import "../global.d.ts"; // eslint-disable-line import-alias/import-alias
 import "../modules.d.ts"; // eslint-disable-line import-alias/import-alias
 
-import { logger } from "@core/logger/index.js";
-
-export * as fonts from "./addons/fonts/FontManager.js";
+export * as fonts from "./addons/fonts";
 export * as plugins from "./addons/plugins";
 export * as themes from "./addons/themes";
 export * as api from "./api";
 export * as ui from "./ui";
 export * as utils from "./utils";
 export * as metro from "@metro";
-
-import * as fonts from "./addons/fonts";
-import * as plugins from "./addons/plugins";
-import * as themes from "./addons/themes";
-import { proxyLazy } from "./utils/lazy";
-
-export const managers = proxyLazy(() => {
-    logger.warn("bunny.managers.* is deprecated, and moved the top level (bunny.*). bunny.managers will be eventually removed soon");
-
-    return {
-        get fonts() { return fonts; },
-        get plugins() { return plugins; },
-        get themes() { return themes; }
-    };
-}, { hint: "object" });
 
 const _disposer = [] as Array<() => unknown>;
 
